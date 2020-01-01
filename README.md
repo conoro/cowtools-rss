358644cb124cba5566f9eab17c91ad40287c3d96
Conor ONeill <conor@conoroneill.com>
2020-01-01 15:17:05 +0000

    first working version. Deployed to Lambda.

---
+++ README.md
@@ -0,0 +1,22 @@
+# Cowtools RSS
+This Serverless function provides an RSS feed for the new The Far Side website.
+
+## Installing and using
+* Configure your AWS account
+* Install Node.js 12+
+
+```bash
+git clone git@github.com:conoro/cowtools-echo-rss.git
+cd cowtools-rss
+npm install -g serverless
+npm install
+serverless deploy
+```
+Then you access the RSS feed like so:
+
+* https://url.of.serverless.function/dev/rss
+
+
+LICENSE Apache-2.0
+
+Copyright Conor O'Neill 2020, conor@conoroneill.com
---
+++ index.js
@@ -0,0 +1,46 @@
+// Cowtools RSS - Copyright Conor O'Neill 2020, conor@conoroneill.com
+// LICENSE Apache-2.0
+// Invoke like https://url.of.serverless.function/dev/rss?page=https://www.eveningecho.ie/corknews or https://url.of.serverless.function/dev/rss?page=https://www.eveningecho.ie/nationalnews or https://url.of.serverless.function/dev/rss?page=https://www.eveningecho.ie/business
+
+module.exports.check = (event, context, callback) => {
+  var request = require("request");
+  var cheerio = require("cheerio");
+  var RSS = require("rss");
+  var URL = "https://www.thefarside.com";
+
+  var feed = new RSS({
+    title: "Cowtools RSS",
+    description: "Return latest comic from The Far Side",
+    feed_url: "http://example.com/rss.xml",
+    site_url: URL,
+    image_url:
+      "https://assets.thefarside.com/assets/packs/media/images/brand/meta_icons/android-chrome-192x192-17a2da94f812f9f4a41ed8ed1be4d889.png",
+    docs: "http://example.com/rss/docs.html",
+    managingEditor: "conor@conoroneill.com",
+    webMaster: "conor@conoroneill.com",
+    copyright: "2020 Conor ONeill",
+    language: "en",
+    pubDate: "Jan 01, 2020 06:00:00 GMT",
+    ttl: "60"
+  });
+
+  request(URL, function(error, response, html) {
+    if (!error && response.statusCode == 200) {
+      var $ = cheerio.load(html);
+      $(".tfs-comic__body").each(function () {
+        var link = $(this).find("img").attr("data-src");
+          var title = $(this).find("figcaption").text();
+          var currentDate = new Date();
+          feed.item({
+            title: title,
+            description: title,
+            url: link,
+            author: "Gary Larson",
+            date: currentDate
+          });
+      });
+      var xml = feed.xml();
+      context.succeed(xml);
+    }
+  });
+};
